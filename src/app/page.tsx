490998ff4a7a90df35651de36aa2ffbcd0d42703
Morksvart <arkantos405@gmail.com>
2024-04-19 19:30:45 +0200

    desktop layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardContent
+  CardContent,
+  CardHeader,
+  CardTitle
 } from "@/components/ui/card"
 import { Pagination, PaginationContent, PaginationItem, PaginationPrevious, PaginationLink, PaginationEllipsis, PaginationNext } from "@/components/ui/pagination";
 
@@ -30,8 +32,29 @@ export default function Home() {
         </PaginationContent>
       </Pagination>
 
-      <Card className="p-2">
-        <CardContent className="sm:bg-[url('/images/bg-sidebar-desktop.svg')] bg-contain bg-no-repeat">
+      <Card className="p-2 flex flex-row">
+        <div className="hidden sm:inline w-1/2 rounded-lg
+        sm:bg-[url('/images/bg-sidebar-desktop.svg')] bg-cover bg-no-repeat">
+          <Pagination>
+            <PaginationContent className="felx flex-col">
+              <PaginationItem >
+                <p>Step 1</p>
+                <PaginationLink className="rounded-full border text-white" href="#">1</PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink className="rounded-full border text-white" href="#">2</PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink className="rounded-full border text-white" href="#">3</PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink className="rounded-full border text-white" href="#">4</PaginationLink>
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
+        </div>
+
+        <CardContent className="p-4 px-16">
           <h2 className="text-lg font-semibold text-gray-900 mb-2">Personal info</h2>
           <p className="text-gray-600 mb-6">Please provide your name, email address, and phone number.</p>
           <div className="mb-4">
@@ -46,9 +69,12 @@ export default function Home() {
             <label htmlFor="phone" className="block text-gray-700 text-sm font-semibold mb-2">Phone Number</label>
             <input type="tel" id="phone" placeholder="e.g. +1 234 567 890" className="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
           </div>
+          <Button className="sm:block hidden float-right bg-marine-blue hover:bg-pastel-blue text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ">
+          Next Step
+        </Button>
         </CardContent>
       </Card>
-      <footer className="absolute inset-x-0 bottom-0 bg-white p-4">
+      <footer className="block sm:hidden fixed inset-x-0 bottom-0 bg-white p-4">
 
         <Button className="float-right bg-marine-blue hover:bg-pastel-blue text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ">
           Next Step
